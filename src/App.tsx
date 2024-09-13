4840ce84547299535a23182afb135c9750d19424
filipigustavo <email@filipigustavo.com.br>
2024-09-13 11:36:30 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+import './App.css';
+import { useState } from 'react';
+
+const formSchema = yup.object({
+  name: yup.string().required().default(''),
+});
+
+function App() {
+  const [showName, setShowName] = useState(false);
+
+  const formik = useFormik({
+    initialValues: formSchema.getDefault(),
+    validationSchema: formSchema,
+    onSubmit: () => setShowName(true),
+    onReset: () => formSchema.getDefault(),
+  });
+
+  const handleClear = () => {
+    formik.resetForm({ values: formSchema.getDefault() });
+    setShowName(false);
+  };
+
+  return (
+    <div>
+      <h1>Digite seu nome</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <input type="text" {...formik.getFieldProps('name')} />
+        <button type="submit" style={{ marginLeft: '8px' }}>
+          Ok
+        </button>
+        <button
+          type="button"
+          style={{ marginLeft: '8px' }}
+          onClick={handleClear}
+        >
+          Limpar
+        </button>
+      </form>
+      {!!showName && <p>Olá, {formik.values.name}</p>}
+    </div>
+  );
+}
+
+export default App;
